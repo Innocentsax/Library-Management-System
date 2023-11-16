7bd990445d6219a118fb6a5d4fb04f15048b70a7
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-16 09:47:43 +0100

    hot fix

--- src/main/java/dev/Innocent/Model/BaseEntity.java
+++ src/main/java/dev/Innocent/Model/BaseEntity.java
@@ -25,29 +25,29 @@ public abstract class BaseEntity<U> {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @Column(name = "library_status", columnDefinition = "varchar(10) default 'ACTIVE'")
-//    @Enumerated(EnumType.STRING)
-//    @Builder.Default
-//    private LibraryStatus libraryStatus = LibraryStatus.ACTIVE;
-//
-//    @Column(name = "created_at", nullable = false, updatable = false)
-//    @CreatedDate
-//    private Timestamp createdAt;
-//
-//    @Column(name = "updated_at")
-//    @LastModifiedDate
-//    private Timestamp updatedAt;
-//
-//    @Column(name = "created_by", updatable = false)
-//    @CreatedBy
-//    private U createdBy;
-//
-//    @Column(name = "updated_by")
-//    @LastModifiedBy
-//    private U updatedBy;
-//
-//    @Column(name = "version")
-//    @Version
-//    @Builder.Default
-//    private Integer version = 0;
+    @Column(name = "library_status", columnDefinition = "varchar(10) default 'ACTIVE'")
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private LibraryStatus libraryStatus = LibraryStatus.ACTIVE;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private Timestamp updatedAt;
+
+    @Column(name = "created_by", updatable = false)
+    @CreatedBy
+    private U createdBy;
+
+    @Column(name = "updated_by")
+    @LastModifiedBy
+    private U updatedBy;
+
+    @Column(name = "version")
+    @Version
+    @Builder.Default
+    private Integer version = 0;
 }
